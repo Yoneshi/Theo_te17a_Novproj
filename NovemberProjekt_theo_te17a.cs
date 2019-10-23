589f211442ec8ef8a302a6ff4a66398cea6db964
Yoneshi <treangpusri@gmail.com>
2019-10-23 14:07:41 +0200

    Initial commit

---
+++ NovemberProjekt_theo_te17a.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theodore_TE17A_Novemberprojektet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
